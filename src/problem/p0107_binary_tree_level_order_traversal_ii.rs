ebe2d632ee21481f604b43663f9271473b13011c
Shenjun Ma <mashenjun0902@gmail.com>
2021-02-19 21:11:17 +0800

    WIP: add problem 102
    Signed-off-by: Shenjun Ma <mashenjun0902@gmail.com>

---
+++ src/problem/p0107_binary_tree_level_order_traversal_ii.rs
@@ -0,0 +1,70 @@
+/**
+ * [107] Binary Tree Level Order Traversal II
+ *
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ *
+ *
+ * For example:<br />
+ * Given binary tree [3,9,20,null,null,15,7],<br />
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ *
+ *
+ * return its bottom-up level order traversal as:<br />
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ *
+ */
+pub struct Solution {}
+use crate::util::tree::{to_tree, TreeNode};
+
+// problem: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+// discuss: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn level_order_bottom(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        vec![]
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_107() {}
+}
