d3048d30b0bbf4748c3ad3ed6afab52ac1cee48d
Shenjun Ma <mashenjun0902@gmail.com>
2021-02-20 02:04:45 +0800

    solve problem 107
    Signed-off-by: Shenjun Ma <mashenjun0902@gmail.com>

--- src/problem/p0107_binary_tree_level_order_traversal_ii.rs
+++ src/problem/p0107_binary_tree_level_order_traversal_ii.rs
@@ -55,7 +55,35 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn level_order_bottom(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
-        vec![]
+        use std::collections::VecDeque;
+
+        let mut result: Vec<Vec<i32>> = vec![];
+        let mut queue: Vec<Rc<RefCell<TreeNode>>> = vec![];
+        match root {
+            Some(node) => {
+                queue.push(node);
+            }
+            None => return result,
+        }
+        while !queue.is_empty() {
+            let mut next_level: Vec<Rc<RefCell<TreeNode>>> = vec![];
+            let mut vals: Vec<i32> = vec![];
+            queue.drain(..).for_each(|mut node| {
+                vals.push(node.borrow().val);
+                let mut node = node.borrow_mut();
+                if let Some(left) = node.left.take() {
+                    next_level.push(left)
+                }
+                if let Some(right) = node.right.take() {
+                    next_level.push(right)
+                }
+            });
+            // replace queue with next_level
+            queue = next_level;
+            result.push(vals);
+        }
+        result.reverse();
+        result
     }
 }
 
@@ -66,5 +94,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_107() {}
+    fn test_107() {
+        let expect: Vec<Vec<i32>> = vec![vec![15, 7], vec![9, 20], vec![3]];
+        assert_eq!(
+            Solution::level_order_bottom(tree![3, 9, 20, null, null, 15, 7]),
+            expect
+        );
+    }
 }
